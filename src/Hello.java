83964e15cc659170ff308426410df59ef7236706
Vida Duodu <vidash50@gmail.com>
2020-11-10 13:17:32 -0500

    initial commit

---
+++ src/Hello.java
@@ -0,0 +1,9 @@
+public class Hello {
+    public static void main(String[] args) {
+
+        // we are practicing how push your codes using inteliJ
+
+        System.out.println("Learning new things everyday");
+
+    }
+}
